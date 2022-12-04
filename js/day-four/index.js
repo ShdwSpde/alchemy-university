86768706a54c117789a8094a8a76795060700183
Mike Spade <mikespadeny@gmail.com>
2022-12-04 13:20:15 -0500

    Update index.js

--- js/day-four/index.js
+++ js/day-four/index.js
@@ -14,3 +14,33 @@ function countC(str) {
     }
     return result
 }
+
+
+function countVowels(string) {
+string = string.toLowerCase()
+let vowelCount = 0
+for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount
+}
+
+
+function reverse(string) {
+    return string.split('').reverse().join('')
+}
+
+
+function isPalindrome(string) {
+return string.split('').reverse().join('') === string ? true:false;
+}
+
+
+function sumTogether(arr1, arr2) {
+for(let i = 0; i < arr1.length;i++){
+    arr1[i] = (arr1[i] + arr2[i])
+}
+return arr1
+}
